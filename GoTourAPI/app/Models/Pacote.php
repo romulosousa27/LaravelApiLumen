dbf5a857cd01a2f282325cae13a5ef6d8f15d426
Romulo Sousa <romulosousa27@hotmail.com>
2019-02-02 20:17:08 -0300

    Pacote: model e migration

---
+++ GoTourAPI/app/Models/Pacote.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Models;
+
+class Pacote extends Model {
+
+    protected $table = 'pacote';
+
+    protected $fillable = [
+        'name', 
+        'price', 
+        'date_start',
+        'date_end',
+        'description',
+        'imagem',
+        'site',
+        'phone',
+    ];
+
+    protected $casts = [
+        'data_start' => 'Timestamps',
+        'data_end' => 'Timestamps',
+    ];
+
+    /**
+     * Remove os campos created_at, updated_at e delete_at da migration
+     */
+    public $timestamps = false; 
+
+}
