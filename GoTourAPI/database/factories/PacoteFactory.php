c6e643d43b37fc113efcd5c76ebda8f6301acec0
Romulo Sousa <romulosousa27@hotmail.com>
2019-02-02 21:18:55 -0300

    Factory Pacote

---
+++ GoTourAPI/database/factories/PacoteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Pacote;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(Pacote::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->text(100),
+        'price' => $faker->randomFloat(2,2,2),
+        'image' => $faker->imageUrl(),
+        'site' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'date_start' => $faker->date(),
+        'date_end' => $faker->date(),
+    ];
+});
