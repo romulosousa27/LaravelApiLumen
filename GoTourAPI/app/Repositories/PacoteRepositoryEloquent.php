6a085ed9ad27485365f80e4b6ba5ba455cf10d2c
Romulo Sousa <romulosousa27@hotmail.com>
2019-02-07 21:07:45 -0300

    Criando repositories

---
+++ GoTourAPI/app/Repositories/PacoteRepositoryEloquent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Pacote;
+use App\Repositories\PacoteRepositoryInterface;
+use Illuminate\Http\Request;
+
+class PacoteRepositoryEloquent implements PacoteRepositoryInterface {
+
+    private $pacote;
+
+    public function __construct(Pacote $pacote){
+        $this->pacote = $pacote;
+    }
+
+    public function searchAllPacotes() {
+        
+        return $this->pacote->select(
+            'id', 'name', 'price', 'date_start', 'date_end', 'image'
+        )->get();
+    }
+
+    public function searchPacote() {
+        
+        return $this->pacote->select(
+            'id', 'name', 'price', 'date_start', 'date_end', 'image'
+        )->where('id', $id)->get();
+    }
+
+    public function searchDetailsPacote(int $id) {
+        
+        return $this->pacote->find($id);
+    }
+
+    public function createPacote(Request $request) {
+        
+        return $this->pacote->create($request->all());
+    }
+
+    public function editPacote(int $id, Request $request) {
+        
+        return $this->pacote->where('id', $id)->update($request->all());
+    }
+
+    public function deletePacote(int $id) {
+        
+        $pacote = Pacote::find($id);
+        return $this->pacote->delete();
+    }
+}
---
+++ GoTourAPI/app/Repositories/PacoteRepositoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Http\Request;
+
+interface PacoteRepositoryInterface {
+
+    
+    public function searchPacotes() {
+        
+    }
+
+    public function searchPacote(int $id) {
+        
+    }
+
+    public function searchDetailsPacote(int $id) {
+        
+    }
+
+    public function createPacote(Request $request) {
+        
+    }
+
+    public function editPacote(int $id, Request $request) {
+        
+    }
+
+    public function deletePacote(int $id) {
+        
+    }
+}
